1891f493d90d150541384405504f339b4ce2e13c
gsempe <guillaume.sempe@gmail.com>
2012-08-25 19:02:08 +0200

    First version of NSString+SYSTExtension category

---
+++ NSString+SYSTExtension.h
@@ -0,0 +1,29 @@
+//
+//  NSString+SYSTExtension.h
+//  SYSTAppleToolbox
+//
+//  Created by Guillaume Sempé on 23/07/12.
+//  Copyright (c) 2012 Syst.ms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SYSTExtension)
+
+/**
+ Choose the right string between the parameters regarding the amount parameter
+ 
+ @param amount The amount of elements targeted 
+ 
+ @discussion The second parameter must be the singular form. The 3rd parameter must be the first plural form and the 4th parameter the second plural form
+ 
+ @return One of the strings given by parameter.
+ */
++ (NSString *)getRightFormForTheAmount:(NSNumber*)amount,...;
+
++ (NSString*)stringWithDeviceToken:(NSData*)deviceToken;
+
+- (NSString *)capitalizedSentence;
+
+- (BOOL)isValidEmail;
+@end
---
+++ NSString+SYSTExtension.m
@@ -0,0 +1,96 @@
+//
+//  NSString+SYSTExtension.m
+//  SYSTAppleToolbox
+//
+//  Created by Guillaume Sempé on 23/07/12.
+//  Copyright (c) 2012 Syst.ms. All rights reserved.
+//
+
+#import "NSString+SYSTExtension.h"
+
+@implementation NSString (SYSTExtension)
+
++ (NSString *)getRightFormForTheAmount:(NSNumber*)amount,...
+{
+    NSString *eachString;
+    NSString *rightString = nil;
+    NSInteger rightForm = NSNotFound;
+    va_list stringList;
+    if(amount) {
+        NSString* language =[[NSLocale preferredLanguages] objectAtIndex:0];
+        NSInteger n = [amount integerValue];
+        if([language isEqualToString:@"pl"]) {  //3
+            rightForm = (n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+        }
+        //        if([language isEqualToString:@"ru"]) {    // 3
+        //            rightForm = (n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+        //        }
+        if(([language isEqualToString:@"fr"]) ||    // 2
+           ([language isEqualToString:@"tr"]) ) {
+            rightForm = (n>1 ? 1 : 0);
+        }
+        if(([language isEqualToString:@"en"]) ||    //2
+           ([language isEqualToString:@"es"]) ||
+           ([language isEqualToString:@"it"]) ||
+           ([language isEqualToString:@"de"]) ||
+           ([language isEqualToString:@"pt"]) ) {
+            rightForm = (n!=1 ? 1 : 0);
+        }
+        if(([language isEqualToString:@"ja"]) ||    //1
+           ([language isEqualToString:@"ko"]) ||
+           ([language isEqualToString:@"zh"]) ){
+            rightForm = 0;
+        }
+        if (NSNotFound==rightForm) {
+            // Use en rule if current language has not be matched
+            rightForm = (n!=1 ? 1 : 0);
+        }
+        NSInteger currentFormIndex = 0;
+        va_start(stringList, amount); // Start scanning for arguments after quantity.
+        while (nil!=(eachString = va_arg(stringList, id))) {
+            if (currentFormIndex == rightForm) {
+                rightString = eachString;
+            }
+            currentFormIndex++;
+        }
+        va_end(stringList);
+    }    
+    return rightString;
+}
+
+- (NSString *)capitalizedSentence
+{
+    if ([self length] >= 1) {
+        return [self stringByReplacingCharactersInRange:NSMakeRange(0, 1) withString:[[self substringToIndex:1] capitalizedString]];
+    } else {
+        return self;
+    }
+}
+
+- (BOOL)isValidEmail
+{
+    BOOL stricterFilter = YES; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
+    NSString *stricterFilterString = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *laxString = @".+@.+\\.[A-Za-z]{2}[A-Za-z]*";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:self];
+}
+
++ (NSString*)stringWithDeviceToken:(NSData*)deviceToken
+{
+    const unsigned *deviceTokenBytes = [deviceToken bytes];
+    NSString *hexDeviceToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                                ntohl(deviceTokenBytes[0]), ntohl(deviceTokenBytes[1]), ntohl(deviceTokenBytes[2]),
+                                ntohl(deviceTokenBytes[3]), ntohl(deviceTokenBytes[4]), ntohl(deviceTokenBytes[5]),
+                                ntohl(deviceTokenBytes[6]), ntohl(deviceTokenBytes[7])];
+    
+    return hexDeviceToken;
+    //    NSString* deviceTokenString = [[[[[deviceToken description]
+    //                                      stringByReplacingOccurrencesOfString: @"<" withString: @""]
+    //                                     stringByReplacingOccurrencesOfString: @">" withString: @""]
+    //                                    stringByReplacingOccurrencesOfString: @" " withString: @""] retain];
+    
+}
+
+@end
