c259b900985a99f3c57a8347b41908c5cadef2f2
gsempe <guillaume.sempe@gmail.com>
2013-02-26 14:39:39 +0100

    Add methods to decode images in background

---
+++ UIImage+SYSTExtension.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+SYSTExtension.h
+//  FotoliaX
+//
+//  Created by Guillaume Sempé on 25/02/13.
+//  Copyright (c) 2013 Fotolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (SYSTExtension)
++ (UIImage *)decodedImageWithImage:(UIImage *)image;
+@end
